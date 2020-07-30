2f6f138a11497a2d72000ca9b0d7a56130105c0c
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-30 00:07:15 -0300

    fix codeclimate issues

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-const styles = [`newspaper`, `magazine1`, `magazine2`];
-const sizes = [`medium`, `big`, `reallybig`];
-const rotates = [`rotateleft`, `rotateright`];
-const skews = [`skewleft`, `skewright`];
+const styles = ['newspaper', 'magazine1', 'magazine2'];
+const sizes = ['medium', 'big', 'reallybig'];
+const rotates = ['rotateleft', 'rotateright'];
+const skews = ['skewleft', 'skewright'];
 const features = [styles, sizes, rotates, skews];
 
 function selectNumberFeatures() {
-    let numberOfFeatures = 1;
-    while (numberOfFeatures < 2) {
-        numberOfFeatures = Math.floor(Math.random() * 4);
-    }
-    return numberOfFeatures;
+  let numberOfFeatures = 1;
+  while (numberOfFeatures < 2) {
+    numberOfFeatures = Math.floor(Math.random() * 4);
+  }
+  return numberOfFeatures;
 }
 
 function selectFeatures(param1) {
-    let selectedFeatures = [];
-    let number = 0;
-    while (number < param1) {
-        let pickArrayFeature = Math.floor(Math.random() * 4);
-        if (!selectedFeatures.includes(pickArrayFeature)) {
-            selectedFeatures.push(pickArrayFeature);
-            number += 1;
-        }
+  let selectedFeatures = [];
+  let number = 0;
+  while (number < param1) {
+    let pickArrayFeature = Math.floor(Math.random() * 4);
+    if (!selectedFeatures.includes(pickArrayFeature)) {
+      selectedFeatures.push(pickArrayFeature);
+      number += 1;
     }
-    return selectedFeatures;
+  }
+  return selectedFeatures;
 }
 
 function selectFeaturesItems(param1) {
-    let selectedFeaturesItems = [];
-    for (item = 0; item < param1.length; item += 1) {
-        const randomNumber = Math.floor(Math.random() * features[param1[item]].length)
-        selectedFeaturesItems.push(features[param1[item]][randomNumber]);
-    }
-    return selectedFeaturesItems;
+  let selectedFeaturesItems = [];
+  for (let item = 0; item < param1.length; item += 1) {
+    const randomNumber = Math.floor(Math.random() * features[param1[item]].length);
+    selectedFeaturesItems.push(features[param1[item]][randomNumber]);
+  }
+  return selectedFeaturesItems;
 }
 
 function creatingArrayFeatures() {
-    let numberFeatures = selectNumberFeatures();
-    let arraySelectedFeatures = selectFeatures(numberFeatures);
-    let arraySelectedFeaturesItems = selectFeaturesItems(arraySelectedFeatures);
-    return arraySelectedFeaturesItems;
+  const numberFeatures = selectNumberFeatures();
+  const arraySelectedFeatures = selectFeatures(numberFeatures);
+  const arraySelectedFeaturesItems = selectFeaturesItems(arraySelectedFeatures);
+  return arraySelectedFeaturesItems;
 }
 
 function creatingCards() {
-    let arrayWords = document.getElementById('cartao-texto').value;
-    for(const item1 in arrayWords.split(' ')) {
-        styling = creatingArrayFeatures();
-        let card = document.createElement('span');
-        for (const item2 in styling) {
-            card.classList.add(styling[item2]);
-        }
-        card.innerText = arrayWords.split(' ')[item1];
-        document.getElementById('carta-gerada').insertAdjacentElement('beforeend', card);
+  const arrayWords = document.getElementById('cartao-texto').value;
+  for (let item1 = 0; item1 < arrayWords.split(' ').length; item1 += 1) {
+    const styling = creatingArrayFeatures();
+    let card = document.createElement('span');
+    for (let item2 = 0; item2 < styling.length; item2 += 1) {
+      card.classList.add(styling[item2]);
     }
+    card.innerText = arrayWords.split(' ')[item1];
+    document.getElementById('carta-gerada').insertAdjacentElement('beforeend', card);
+  }
 }
 
 window.onload = function () {
-    document.getElementById('criar-carta').addEventListener('click', creatingArrayFeatures);
-    document.getElementById('cartao-texto').addEventListener('change', creatingCards);
+  document.getElementById('criar-carta').addEventListener('click', creatingArrayFeatures);
+  document.getElementById('cartao-texto').addEventListener('change', creatingCards);
 }
