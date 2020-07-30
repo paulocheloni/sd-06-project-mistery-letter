a5017276cdb6680c5bc350cbdd7767f8153eb3e8
Rods <rods.leite27@gmail.com>
2020-07-30 15:13:15 -0300

    Requisitos 3, 5, 6 e ate o 15

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const botaoCriar = document.getElementById('criar-carta')
-const inputCarta = document.getElementById('carta-texto')
-const outputCarta = document.getElementById('carta-gerada')
+const botaoCriar = document.getElementById('criar-carta');
+const inputCarta = document.getElementById('carta-texto');
+const outputCarta = document.getElementById('carta-gerada');
 
 //Faz o texto aparecer no parágrafo
 botaoCriar.addEventListener('click', function () {
-  outputCarta.innerText = inputCarta.value
-})
+  outputCarta.childNodes[1].innerText = inputCarta.value;
+  if (outputCarta.childNodes[1].innerText == "" || outputCarta.childNodes[1].innerText.match(/^\s*$/)) {
+    outputCarta.childNodes[1].innerText = 'Por favor, digite o conteúdo da Carta.'
+  }
+});
+
+
