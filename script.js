b5975f0b1a890736d19dedc05ba930d63857f881
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-29 22:12:14 -0300

    Fixing Codeclimate issues

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const functionalities = {
     const textToManipulate = inputTextElement.value;
     let occurrenceOfWhiteSpaceInARow = 0;
     while (textToManipulate[occurrenceOfWhiteSpaceInARow] === ' ') {
-      occurrenceOfWhiteSpaceInARow +=1 ;
+      occurrenceOfWhiteSpaceInARow += 1;
     }
     if (occurrenceOfWhiteSpaceInARow !== textToManipulate.length && textToManipulate.value !== '') {
-    functionalities.editLetterStyle(textToManipulate);
+      functionalities.editLetterStyle(textToManipulate);
     } else {
       document.querySelector('#carta-gerada').innerText = 'Por favor, digite o conteúdo da carta!';
     }
@@ -19,43 +19,42 @@ const functionalities = {
     let i = 0;
     let word = '';
     let wordCounter = 0;
-    while(i < textToManipulate.length) {
-      word += textToManipulate[i].replace(' ','');
-      if (textToManipulate[i] === ' ' || i === textToManipulate.length-1) {
+    while (i < textToManipulate.length) {
+      word += textToManipulate[i].replace(' ', '');
+      if (textToManipulate[i] === ' ' || i === textToManipulate.length - 1) {
         const spanElement = document.createElement('span');
         spanElement.className = functionalities.generateRandomStyle();
         spanElement.innerHTML = word;
         letterElement.appendChild(spanElement);
-        if (i !== textToManipulate.length-1) {
+        if (i !== textToManipulate.length - 1) {
           letterElement.innerHTML = letterElement.innerHTML.concat(' ');
         }
         word = '';
         wordCounter += 1;
-        spanElement.addEventListener('click' , functionalities.changeClass);
+        spanElement.addEventListener('click', functionalities.changeClass);
       }
       i += 1;
     }
     const wordCounterElement = document.querySelector('#carta-contador');
-    //wordCounterElement.innerHTML = (('esta é uma carta com ').concat(wordCounter)).concat(' palavras');
     wordCounterElement.innerHTML = wordCounter;
   },
-  generateRandomStyle: function generateRandomStyle () {
-    let classes = [
+  generateRandomStyle: function generateRandomStyle() {
+    const classes = [
       ['newspaper', 'magazine1', 'magazine2'], // Style Group
       ['medium', 'big', 'reallybig'], // Size Group
       ['rotateleft', 'rotateright'], // Rotation Group
       ['skewleft', 'skewright'], // Inclination Group
     ];
-    const numberOfClasses = Math.round(Math.random() * (4 - 2) + 2); // It can be 2, 3 or 4
+    const numberOfClasses = Math.round((Math.random() * (4 - 2)) + 2); // It can be 2, 3 or 4
     let classString = '';
     for (let i = 0; i < numberOfClasses; i += 1) {
-      const randomGroup = Math.round(Math.random() * (4-i - 1) + 1);
-      const classInsideGroup = Math.round(Math.random() * (classes[randomGroup-1].length-1) +1);
-      classString = classString.concat(classes[randomGroup-1][classInsideGroup-1]);
-      if (i !== numberOfClasses-1) {
+      const randomGroup = Math.round((Math.random() * (4 - i - 1)) + 1);
+      const classInsideGroup = Math.round((Math.random() * (classes[randomGroup - 1].length - 1)) +1);
+      classString = classString.concat(classes[randomGroup - 1][classInsideGroup - 1]);
+      if (i !== numberOfClasses - 1) {
         classString = classString.concat(' ');
       }
-      classes.splice(randomGroup-1, 1); // Avoid error selecting a same group twice or more - delete group row
+      classes.splice(randomGroup - 1, 1); // Avoid select same group twice or + (delete group row)
     }
     return classString;
   },
@@ -69,5 +68,5 @@ window.onload = function () {
   const buttonCreateElement = document.querySelector('#criar-carta');
 
   // Events
-buttonCreateElement.addEventListener('click', functionalities.verifyAndEdit);
+  buttonCreateElement.addEventListener('click', functionalities.verifyAndEdit);
 };
