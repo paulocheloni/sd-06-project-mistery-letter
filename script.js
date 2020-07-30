b145467bcc4f10297d0dab811b6a7ba2ec1f1ff6
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-30 17:35:05 -0300

    Funcionalidade de exigir que um texto seja inserido

---
+++ script.js
@@ -0,0 +1,15 @@
+const boxPhrase = document.querySelector('#carta-texto');
+const buttonGerarCarta = document.querySelector('#criar-carta');
+
+function gerarCarta() {
+  if(boxPhrase.value === "") {
+    document.getElementById('carta-gerada').innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    console.log(boxPhrase.value)
+  }
+}
+
+buttonGerarCarta.addEventListener("click", gerarCarta);
+
+
+
