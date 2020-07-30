e72bc263366dc65658ffb73b0aabadc9be885b9c
Marxneves <marxkhristie@gmail.com>
2020-07-30 16:07:58 -0300

    criando classe e gerando palavra estilizada

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-window.onload = {
+const input = document.getElementById('carta-texto');
+const btnGenerate = document.getElementById('criar-carta');
+const paragraph = document.getElementById('carta-gerada');
+const arrayClass = ['newspaper','magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright']; 
 
-};
+function takeClass() {
+  let classes = arrayClass;
+  let random = Math.floor(Math.random() * 10);
+  return classes[random];
+}
+
+btnGenerate.addEventListener('click', () => {
+  let string = input.value;
+  string = string.split(' ');
+
+  if (input.value.trim() == "") {
+    paragraph.innerHTML ='Por favor, digite o conteúdo da carta.';
+  } else {  
+    for (let i = 0; i < string.length; i += 1) {
+      const span = document.createElement('span');
+      span.innerHTML = string[i];
+      span.className = takeClass();
+      paragraph.appendChild(span);
+    }
+  }
+});
+
+paragraph.addEventListener('click', (event) => {
+  const palavra = event.target;
+  palavra.className = takeClass();
+});
