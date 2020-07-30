247f82103dcaef34b882aa78b6c7151f96f3ca90
Rods <rods.leite27@gmail.com>
2020-07-30 15:31:11 -0300

    Correcao Requisitos 3 e 5

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const outputCarta = document.getElementById('carta-gerada');
 
 //Faz o texto aparecer no parágrafo
 botaoCriar.addEventListener('click', function () {
-  outputCarta.childNodes[1].innerText = inputCarta.value;
-  if (outputCarta.childNodes[1].innerText == "" || outputCarta.childNodes[1].innerText.match(/^\s*$/)) {
-    outputCarta.childNodes[1].innerText = 'Por favor, digite o conteúdo da carta.'
+  let span = document.createElement('span')
+  outputCarta.appendChild(span)
+  span.innerText = inputCarta.value;
+  if (span.innerText == "" || span.innerText.match(/^\s*$/)) {
+    span.innerText = 'Por favor, digite o conteúdo da carta!'
   }
 });
 
