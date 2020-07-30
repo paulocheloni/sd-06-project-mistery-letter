603c222088529c4dd6f925589c0e7b3700c8360c
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-30 17:43:25 -0300

    Requisito4

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const letter = document.querySelector('#carta-gerada');
 const letterText = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
-button.addEventListener('click', function (){
-  let span = document.createElement('span');
+button.addEventListener('click', function () {
+  const span = document.createElement('span');
   letter.appendChild(span);
+  if (letterText.value.length === 0 || letterText.value.trim() === "") {
+    alert('Por favor, digite o conteúdo da carta.');
+  }
 });
