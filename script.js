3acc52702c5a6b7d9d1d6632ed0b76c5371652b3
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-30 18:21:59 -0300

    correçoes

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const letter = document.querySelector('#carta-gerada');
 const letterText = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
 button.addEventListener('click', function () {
-  const span = document.createElement('span');
-  letter.appendChild(span);
-  if (letterText.value.length === 0 || letterText.value.trim() === "") {
-    alert('Por favor, digite o conteúdo da carta.');
+  arrayStrings = letterText.value.split(' ');
+  if (letterText.value.length === 0 || letterText.value.trim() === '') {
+    letter.innerHTML = ('Por favor, digite o conteúdo da carta.');
+  } else {
+    for (let i = 0; i < arrayStrings.length; i += 1) {
+      const span = document.createElement('span');
+      span.innerHTML = arrayStrings[i];
+      letter.appendChild(span);
+    }
   }
+  console.log(arrayStrings.length);
 });
