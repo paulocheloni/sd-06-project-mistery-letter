f70f4a146e5f30d25df8a1500bd64f7ca7a6f969
Rods <rods.leite27@gmail.com>
2020-07-30 16:03:08 -0300

    Correcao Requisitos 3

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const outputCarta = document.getElementById('carta-gerada');
 
 //Faz o texto aparecer no parágrafo
 botaoCriar.addEventListener('click', function () {
-  let span = document.createElement('span')
-  outputCarta.appendChild(span)
-  span.innerText = inputCarta.value;
-  if (span.innerText == "" || span.innerText.match(/^\s*$/)) {
-    span.innerText = 'Por favor, digite o conteúdo da carta!'
+  for (let i = 0; i < inputCarta.value.length; i+= 1) {
+    let span = document.createElement('span')
+    outputCarta.appendChild(span)
+    span.innerText += inputCarta.value[i];
+    outputCarta.appendChild(span)
   }
 });
 
+/*function addClass (event){
+let span = event.target
+var classes = ['newspaper', 'medium', 'rotateleft', 'skewleft']
+}*/
 
+/*if (outputCarta.innerText == "" || outputCarta.match(/^\s*$/)) {
+  outputCarta.innerText = 'Por favor, digite o conteúdo da carta!'
+}
+}*/
