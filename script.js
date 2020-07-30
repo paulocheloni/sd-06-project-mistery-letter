e262abd50780b462efaf8308073a272bc096db3b
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-30 17:47:45 -0300

    BUG da recusa de texto vazio e da classe magazine 2

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ const boxPhrase = document.querySelector('#carta-texto');
 const buttonGerarCarta = document.querySelector('#criar-carta');
 
 function gerarCarta() {
-  if(boxPhrase.value === "") {
+  if (boxPhrase.value === '') {
     document.getElementById('carta-gerada').innerHTML = 'Por favor, digite o conteúdo da carta.';
   } else {
     console.log(boxPhrase.value)
   }
 }
 
-buttonGerarCarta.addEventListener("click", gerarCarta);
-
-
-
+buttonGerarCarta.addEventListener('click', gerarCarta);
