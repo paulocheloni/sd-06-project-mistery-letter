ddf06d6048b7343387b5ad797dc0aebb55dd1bc9
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-30 16:52:13 -0300

    testing classes

--- script.js
+++ script.js
@@ -1,7 +1,73 @@
 const createLetterButton = document.getElementById('criar-carta');
 const inputText = document.getElementById('carta-texto');
-let letterGenerated = document.getElementById('carta-gerada');
+const letterGenerated = document.getElementById('carta-gerada');
 
-createLetterButton.addEventListener('click', function() {
-  letterGenerated.innerHTML = inputText.value;
+function randomStyleGroup() {
+  const class0 = 0;
+  const class1 = 1;
+  const randomClass = Math.round(Math.random() * 3);
+  if (class0 === randomClass) {
+    return 'newspaper';
+  } else if (class1 === randomClass) {
+    return 'magazine1';
+  } else {
+    return 'magazine2';
+  }
+}
+
+function randomSizeGroup() {
+  const class0 = 0;
+  const class1 = 1;
+  const randomClass = Math.round(Math.random() * 3);
+  if (class0 === randomClass) {
+    return 'medium';
+  } else if (class1 === randomClass) {
+    return 'big';
+  } else {
+    return 'reallybig';
+  }
+}
+
+function randomRotationGroup() {
+  const class0 = 0;
+  const randomClass = Math.round(Math.random() * 2);
+  if (class0 === randomClass) {
+    return 'rotateleft';
+  } else {
+    return 'rotateright';
+  }
+}
+
+function randomSkewGroup() {
+  const class0 = 0;
+  const randomClass = Math.round(Math.random() * 2);
+  if (class0 === randomClass) {
+    return 'skewleft';
+  } else {
+    return 'skewright';
+  }
+}
+
+createLetterButton.addEventListener('click', function () {
+  letterGenerated.innerHTML = '';
+  const stringArray = inputText.value.split(' ');
+  for (let i = 0; i < stringArray.length; i += 1) {
+    const spanTag = document.createElement('span');
+    const space = ' ';
+    if (i !== stringArray.length - 1) {
+      spanTag.innerHTML = stringArray[i] + space;
+      spanTag.className += randomStyleGroup();
+      spanTag.className += space + randomSizeGroup();
+      spanTag.className += space + randomRotationGroup();
+      spanTag.className += space + randomSkewGroup();
+    } else {
+      spanTag.innerHTML = stringArray[i];
+      spanTag.className += randomStyleGroup();
+      spanTag.className += space + randomSizeGroup();
+      spanTag.className += space + randomRotationGroup();
+      spanTag.className += space + randomSkewGroup();
+    }
+    letterGenerated.appendChild(spanTag);
+  }
+  console.log(letterGenerated);
 });
