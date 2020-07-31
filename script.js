f91a4a187d826409ecb9d6b170ca0cc63e8d8f84
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-31 17:13:48 -0300

    Correções simples de CodeClimate.

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const cartaTexto = document.querySelector('#carta-texto');
 const grupoEstilo = {
   0: 'newspaper',
   1: 'magazine1',
-  2: 'magazine2'
+  2: 'magazine2',
 };
 const grupoTamanho = {
   0: 'medium',
   1: 'big',
-  2: 'reallybig'
+  2: 'reallybig',
 };
 const grupoRotacao = {
   0: 'rotateleft',
-  1: 'rotateright'
-}
+  1: 'rotateright',
+};
 const grupoInclinacao = {
   0: 'skewleft',
-  1: 'skewright'
-}
+  1: 'skewright',
+};
 
 function zeroOuUm() {
   const selecionaQual = Math.floor(Math.random() * 2);
@@ -33,44 +33,39 @@ function zeroADois() {
 function selecionaEstilo() {
   if (zeroADois() === 0) {
     return '';
-  } else {
-    return grupoEstilo[zeroADois()];
   }
+  return grupoEstilo[zeroADois()];
 }
 
 function selecionaTamanho() {
   if (zeroADois() === 0) {
     return '';
-  } else {
-    return grupoTamanho[zeroADois()];
   }
+  return grupoTamanho[zeroADois()];
 }
 
 function selecionaRotacao() {
   if (zeroADois() === 0) {
     return '';
-  } else {
-    return grupoRotacao[zeroOuUm()];
   }
+  return grupoRotacao[zeroOuUm()];
 }
 
 function selecionaInclinacao() {
   if (zeroADois() === 0) {
     return '';
-  } else {
-    return grupoInclinacao[zeroOuUm()];
   }
+  return grupoInclinacao[zeroOuUm()];
 }
 
 function criarCarta() {
-  valorSalvo = cartaTexto.value;
-  cartaTexto.value += ' ';
-  let textoRestante = cartaTexto.value;
-  let palavra = '';
+  const valorSalvo = cartaTexto.value;
+  let textoRestante = cartaTexto.value += ' ';
 
   if (textoRestante.length < 1 || textoRestante.trim() === '') {
     quadroDeCartas.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
+    let palavra = '';
     quadroDeCartas.innerText = '';
     for (let x = 0; x < textoRestante.length; x += 1) {
       if (textoRestante[x] !== ' ') {
