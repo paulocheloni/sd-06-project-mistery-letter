38fd19785c4eb36ea37fba4aacf7a61ccc0c9de0
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-31 17:03:25 -0300

    Adicionadas classes de forma aleatória conforme etapa 16.

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
 const botaoGerarCarta = document.querySelector('#gerar-carta');
 const quadroDeCartas = document.querySelector('#carta-gerada');
 const cartaTexto = document.querySelector('#carta-texto');
+const grupoEstilo = {
+  0: 'newspaper',
+  1: 'magazine1',
+  2: 'magazine2'
+};
+const grupoTamanho = {
+  0: 'medium',
+  1: 'big',
+  2: 'reallybig'
+};
+const grupoRotacao = {
+  0: 'rotateleft',
+  1: 'rotateright'
+}
+const grupoInclinacao = {
+  0: 'skewleft',
+  1: 'skewright'
+}
+
+function zeroOuUm() {
+  const selecionaQual = Math.floor(Math.random() * 2);
+  return selecionaQual;
+}
+
+function zeroADois() {
+  const selecionaQual = Math.floor(Math.random() * 3);
+  return selecionaQual;
+}
+
+function selecionaEstilo() {
+  if (zeroADois() === 0) {
+    return '';
+  } else {
+    return grupoEstilo[zeroADois()];
+  }
+}
+
+function selecionaTamanho() {
+  if (zeroADois() === 0) {
+    return '';
+  } else {
+    return grupoTamanho[zeroADois()];
+  }
+}
+
+function selecionaRotacao() {
+  if (zeroADois() === 0) {
+    return '';
+  } else {
+    return grupoRotacao[zeroOuUm()];
+  }
+}
+
+function selecionaInclinacao() {
+  if (zeroADois() === 0) {
+    return '';
+  } else {
+    return grupoInclinacao[zeroOuUm()];
+  }
+}
 
 function criarCarta() {
   valorSalvo = cartaTexto.value;
@@ -18,7 +78,7 @@ function criarCarta() {
       } else {
         const novaCarta = document.createElement('span');
         novaCarta.innerText = palavra;
-        novaCarta.className = 'carta';
+        novaCarta.className = `${selecionaEstilo()} ${selecionaTamanho()} ${selecionaRotacao()} ${selecionaInclinacao()} carta`;
         quadroDeCartas.appendChild(novaCarta);
 
         textoRestante = textoRestante.substr(palavra.length + 1);
