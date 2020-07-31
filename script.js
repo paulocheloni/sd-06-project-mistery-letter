1edf631ee7690d7b8e4c758fefa2dad1067152cb
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-31 14:05:13 -0300

    ultimo requisito

--- script.js
+++ script.js
@@ -2,19 +2,43 @@ const contador = document.querySelector('#carta-contador');
 const letter = document.querySelector('#carta-gerada');
 const letterText = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
-button.addEventListener('click', function () {
-  arrayStrings = letterText.value.split(' ');
-  if (letterText.value.length === 0 || letterText.value.trim() === '') {
-    letter.innerHTML = ('por favor, digite o conteúdo da carta!');
-  } else {
-    while (letter.firstChild) {
-      letter.removeChild(letter.firstChild);
+const array1 = ['newspaper', 'magazine1', 'magazine2'];
+const array2 = ['medium', 'big', 'reallybig'];
+const array3 = ['rotateleft', 'rotateright'];
+const array4 = ['skewleft', 'skewright'];
+
+function styleChoice() {
+  var concat = [];
+  let arrayChoice2 = 0;
+  for (let i = 0; i < 2; i += 1) {
+    let arrayChoice = `array${Math.floor(Math.random() * 4) + 1}`;
+    while (arrayChoice != arrayChoice2) {
+      if (arrayChoice === 'array1' || arrayChoice === 'array2'){
+        var style = Math.floor(Math.random() * 3);
+      } else {
+        var style = Math.floor(Math.random() * 2);
+      }
+      concat.push(arrayChoice + '[' + style + ']');
+      arrayChoice2 = arrayChoice;
     }
-    for (let i = 0; i < arrayStrings.length; i += 1) {
-      const span = document.createElement('span');
-      span.innerHTML = arrayStrings[i];
-      letter.appendChild(span);
-    }
+    }  
+    return concat;
+  }
+  button.addEventListener('click', function () {
+    let arrayStrings = letterText.value.split(' ');
+    if (letterText.value.length === 0 || letterText.value.trim() === '') {
+      letter.innerHTML = ('por favor, digite o conteúdo da carta!');
+    } else {
+      while (letter.firstChild) {
+        letter.removeChild(letter.firstChild);
+      }
+      for (let i = 0; i < arrayStrings.length; i += 1) {
+        const span = document.createElement('span');
+        span.innerHTML = arrayStrings[i];
+        span.classList.add(eval(styleChoice()[0]));
+        span.classList.add(eval(styleChoice()[1]));        
+        letter.appendChild(span);
+      }
   }
   contador.innerHTML = arrayStrings.length;
 });
