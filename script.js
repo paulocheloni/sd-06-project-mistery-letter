cce7ccb4a6c42459b0ac8f0e5b764bcce0e12003
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-01 00:18:46 -0300

    Task 5 Fixes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('criar-carta').addEventListener('click', function () {
+  document.getElementById('carta-gerada').innerText = '';
   let cont = 0;
   for (let i = 0; i < document.getElementById('carta-texto').value.length; i += 1) {
     if (document.getElementById('carta-texto').value[i] !== ' ') {
@@ -11,8 +12,8 @@ document.getElementById('criar-carta').addEventListener('click', function () {
       const misteryWord = document.createElement('span');
       misteryWord.innerText = misteryArray[i];
       document.getElementById('carta-gerada').appendChild(misteryWord);
-    } 
-  } else {
-      alert('Por favor, digite o conteúdo da carta.');
     }
+  } else {
+    document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
+  }
 });
