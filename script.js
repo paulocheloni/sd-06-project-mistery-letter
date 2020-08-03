aee79dce63573b5ef80f148f3c1c3472df81eed6
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-03 00:42:11 -0300

    Task 16 Fixes

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const stylesGroup = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
 const numberOfWords = document.getElementById('carta-contador');
 
-function addStyles(word) {
+function addStyle1(word) {
+  const style1 = Math.floor(Math.random() * 2.7);
+  word.className += `${stylesGroup[style1]} `;
+}
 
+function addStyle2(word) {
+  const style2 = Math.floor((Math.random() * 2.7) + 3);
+  word.className += `${stylesGroup[style2]} `;
+}
+
+function addStyle3(word) {
+  const style3 = Math.floor((Math.random() * 1.7) + 6);
+  word.className += `${stylesGroup[style3]} `;
+}
+
+function addStyle4(word) {
+  const style4 = Math.floor((Math.random() * 1.7) + 8);
+  word.className = `${stylesGroup[style4]} `;
+}
+
+function addStyles(word) {
+  const numberOfStyles = Math.floor((Math.random() * 3.7) + 1);
+  if (numberOfStyles === 1) {
+    addStyle1(word);
+  } else if (numberOfStyles === 2) {
+    addStyle1(word);
+    addStyle2(word);
+  } else if (numberOfStyles === 3) {
+    addStyle1(word);
+    addStyle2(word);
+    addStyle3(word);
+  } else {
+    addStyle1(word);
+    addStyle2(word);
+    addStyle3(word);
+    addStyle4(word);
+  }
 }
 
 document.getElementById('criar-carta').addEventListener('click', function () {
