0e3c6881034baaefe4d8e1a6cc0a37642f3c1cbd
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-03 02:15:43 -0300

    Task 16 Fixes 4

--- script.js
+++ script.js
@@ -1,60 +1,52 @@
-const stylesGroup = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+const groupStyle = ['newspaper', 'magazine1', 'magazine2'];
+const groupSize = ['medium', 'big', 'reallybig'];
+const groupRotate = ['rotateleft', 'rotateright'];
+const groupSkew = ['skewleft', 'skewright'];
 const numberOfWords = document.getElementById('carta-contador');
-let miniMisteryArray = [];
-let letterArray = [];
 
-function addOneStyle() {
-  const style1 = Math.floor(Math.random() * 2.7);
-  miniMisteryArray.push(stylesGroup[style1]);
+function addOneStyle(word) {
+  const style1 = Math.floor(Math.random() * 3);
+  word.classList.add(groupStyle[style1]);
 }
 
-function addTwoStyles() {
-  const style1 = Math.floor(Math.random() * 2.7);
-  miniMisteryArray.push(stylesGroup[style1]);
-  const style2 = Math.floor((Math.random() * 2.7) + 3);
-  miniMisteryArray.push(stylesGroup[style2]);
+function addTwoStyles(word) {
+  const style1 = Math.floor(Math.random() * 3);
+  word.classList.add(groupStyle[style1]);
+  const style2 = Math.floor(Math.random() * 2);
+  word.classList.add(groupSize[style2]);
 }
 
-function addThreeStyles() {
-  const style1 = Math.floor(Math.random() * 2.7);
-  miniMisteryArray.push(stylesGroup[style1]);
-  const style2 = Math.floor((Math.random() * 2.7) + 3);
-  miniMisteryArray.push(stylesGroup[style2]);
-  const style3 = Math.floor((Math.random() * 1.7) + 6);
-  miniMisteryArray.push(stylesGroup[style3]);
+function addThreeStyles(word) {
+  const style1 = Math.floor(Math.random() * 3);
+  word.classList.add(groupStyle[style1]);
+  const style2 = Math.floor(Math.random() * 3);
+  word.classList.add(groupSize[style2]);
+  const style3 = Math.floor(Math.random() * 2);
+  word.classList.add(groupRotate[style3]);
 }
 
-function addFourStyles() {
-  const style1 = Math.floor(Math.random() * 2.7);
-  miniMisteryArray.push(stylesGroup[style1]);
-  const style2 = Math.floor((Math.random() * 2.7) + 3);
-  miniMisteryArray.push(stylesGroup[style2]);
-  const style3 = Math.floor((Math.random() * 1.7) + 6);
-  miniMisteryArray.push(stylesGroup[style3]);
-  const style4 = Math.floor((Math.random() * 1.7) + 8);
-  miniMisteryArray.push(stylesGroup[style4]);
-}
-
-function generateMiniArray() {
-  const numberOfStyles = Math.ceil(Math.random() * 4);
-  if (numberOfStyles === 1) {
-    addOneStyle();   
-  } else if (numberOfStyles === 2) {
-    addTwoStyles();
-  } else if (numberOfStyles === 3) {
-    addThreeStyles();
-  } else {
-    addFourStyles();
-  }
+function addFourStyles(word) {
+  const style1 = Math.floor(Math.random() * 3);
+  word.classList.add(groupStyle[style1]);
+  const style2 = Math.floor(Math.random() * 3);
+  word.classList.add(groupSize[style2]);
+  const style3 = Math.floor(Math.random() * 2);
+  word.classList.add(groupRotate[style3]);
+  const style4 = Math.floor(Math.random() * 2);
+  word.classList.add(groupSkew[style4]);
 }
 
 function addStyles(word) {
-  generateMiniArray();
-  for (let i = 0; i < miniMisteryArray.length; i += 1) {
-    word.className += `${miniMisteryArray[i]} `;
+  const numberOfStyles = Math.ceil(Math.random() * 4);
+  if (numberOfStyles === 1) {
+    addOneStyle(word); 
+  } else if (numberOfStyles === 2) {
+    addTwoStyles(word);
+  } else if (numberOfStyles === 3) {
+    addThreeStyles(word);
+  } else {
+    addFourStyles(word);
   }
-  letterArray.push(miniMisteryArray);
-  miniMisteryArray = [];
 }
 
 document.getElementById('criar-carta').addEventListener('click', function () {
