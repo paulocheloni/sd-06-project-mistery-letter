798b6ad72e0aacfd6e083cde44d7c2cb72f55c9b
Alvaro Neto <alvaroshn@gmail.com>
2020-08-03 13:46:46 -0300

    passo 04

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ let viewLetter = document.querySelector('#carta-gerada');
 btnCreateLetter.addEventListener('click' , createLetter);
 
 //Criação das Funções
-
 function createLetter(){
     clearAll();
-        let separetedPhrases = inputLetter.value.split(' ');;
+        let separetedPhrases = inputLetter.value.split(' ');
         if (inputLetter.value.trim() === '') {
             let spanTextError = document.createElement('p');
             spanTextError.id = "errorID";
@@ -18,30 +17,38 @@ function createLetter(){
             viewLetter.appendChild(spanTextError);
             alert("Por favor, digite o conteúdo da carta.");
         } else {
+            let countLetters = document.getElementById('carta-contador');
             for (let j = 0 ; j < separetedPhrases.length ; j += 1){
-            let spanText = document.createElement('span');
-            spanText.innerText = separetedPhrases[j];
+                let spanText = document.createElement('span');
+                spanText.innerText = separetedPhrases[j];
                 for (let i = 0 ; i < 4 ; i += 1) {
                     spanText.classList.add(randomClassGenerator()[i]);
                 }
-            spanText.addEventListener('click' , function () {
+                spanText.addEventListener('click' , function () {
                 spanText.classList.value = '';
                 for (let i = 0 ; i < 4 ; i += 1) {
                     spanText.classList.add(randomClassGenerator()[i]);
                 }
             });
             viewLetter.appendChild(spanText);
+            countLetters.innerText = separetedPhrases.length;
             }            
         }
-    
 }
 
 function clearAll(){
     let checkIfThereIsAnyTag = document.getElementsByTagName('span');
     let checkIfThereIsAnyError = document.getElementById('errorID');
+    let checkIfThereIsAnyCount = document.getElementById('carta-contador');
+
+    if (checkIfThereIsAnyCount !== null){
+        checkIfThereIsAnyCount.innerText = "";
+    }
+
     if (checkIfThereIsAnyError !== null){
         checkIfThereIsAnyError.remove();
     }
+
     while(checkIfThereIsAnyTag.length !== 0){
         checkIfThereIsAnyTag[0].remove();
     }
