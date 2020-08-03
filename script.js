17be664aa5d21b73487afd60e98ae77dadf6a676
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-03 00:09:26 -0300

    Bonus Task 2

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 const stylesGroup = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+const numberOfWords = document.getElementById('carta-contador');
 
-function addStyle(word) {
-  const stylesNumber = Math.ceil(Math.random() * 4) + 1;
-  let groupStyle = stylesGroup.slice(0, 10);
-  for (let i = 1; i <= stylesNumber; i += 1) {
-    const addStyle = Math.ceil(Math.random() * groupStyle.length);
-    word.className += `${groupStyle[addStyle]} `;
-    if (groupStyle[addStyle] === 'newspaper' || groupStyle[addStyle] === 'magazine1' || groupStyle[addStyle] === 'magazine2') {
-      groupStyle.splice(groupStyle.indexOf('newspaper'),3);
-    } else if (groupStyle[addStyle] === 'medium' || groupStyle[addStyle] === 'big' || groupStyle[addStyle] === 'reallybig') {
-      groupStyle.splice(groupStyle.indexOf('medium', 3));
-    } else if (groupStyle[addStyle] === 'rotateleft' || groupStyle[addStyle] === 'rotateright') {
-      groupStyle.splice(groupStyle.indexOf('rotateleft', 2));
-    } else {
-      groupStyle.splice(groupStyle.indexOf('skewlef', 2));
-    }
-  }
-  groupStyle = stylesGroup.slice(0, 10);
+function addStyles(word) {
+
 }
 
 document.getElementById('criar-carta').addEventListener('click', function () {
@@ -29,12 +15,17 @@ document.getElementById('criar-carta').addEventListener('click', function () {
   }
   if (document.getElementById('carta-texto').value !== '' && cont > 0) {
     const misteryArray = document.getElementById('carta-texto').value.split(' ');
+    let cont = 0;
     for (let i = 0; i < misteryArray.length; i += 1) {
-      const misteryWord = document.createElement('span');
-      misteryWord.innerText = misteryArray[i];
-      addStyle(misteryWord);
-      document.getElementById('carta-gerada').appendChild(misteryWord);
+      if (misteryArray[i] !== '') {  
+        const misteryWord = document.createElement('span');
+        misteryWord.innerText = misteryArray[i];
+        addStyles(misteryWord);
+        document.getElementById('carta-gerada').appendChild(misteryWord);
+        cont += 1;
+      }
     }
+    numberOfWords.innerText = `Total de Palavras : ${cont}`;
   } else {
     document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
   }
