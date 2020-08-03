c96f8bf0b589a10af8511ccdede5ad5ba6ec08bd
Alvaro Neto <alvaroshn@gmail.com>
2020-08-03 13:36:23 -0300

    passo 03

--- script.js
+++ script.js
@@ -9,27 +9,41 @@ btnCreateLetter.addEventListener('click' , createLetter);
 //Criação das Funções
 
 function createLetter(){
-    let separetedPhrases = inputLetter.value.split(' ');;
-    if (inputLetter.value.trim() === '') {
-        let spanTextError = document.createElement('span');
-        spanTextError.innerText = "Por favor, digite o conteúdo da carta.";
-        viewLetter.appendChild(spanTextError);
-        alert("Por favor, digite o conteúdo da carta.");
-    } else {
-        for (let j = 0 ; j < separetedPhrases.length ; j += 1){
-        let spanText = document.createElement('span');
-        spanText.innerText = separetedPhrases[j];
-            for (let i = 0 ; i < 4 ; i += 1) {
-                spanText.classList.add(randomClassGenerator()[i]);
-            }
-        spanText.addEventListener('click' , function () {
-            spanText.classList.value = '';
-            for (let i = 0 ; i < 4 ; i += 1) {
-                spanText.classList.add(randomClassGenerator()[i]);
-            }
-        });
-        viewLetter.appendChild(spanText);
-        }            
+    clearAll();
+        let separetedPhrases = inputLetter.value.split(' ');;
+        if (inputLetter.value.trim() === '') {
+            let spanTextError = document.createElement('p');
+            spanTextError.id = "errorID";
+            spanTextError.innerText = "Por favor, digite o conteúdo da carta.";
+            viewLetter.appendChild(spanTextError);
+            alert("Por favor, digite o conteúdo da carta.");
+        } else {
+            for (let j = 0 ; j < separetedPhrases.length ; j += 1){
+            let spanText = document.createElement('span');
+            spanText.innerText = separetedPhrases[j];
+                for (let i = 0 ; i < 4 ; i += 1) {
+                    spanText.classList.add(randomClassGenerator()[i]);
+                }
+            spanText.addEventListener('click' , function () {
+                spanText.classList.value = '';
+                for (let i = 0 ; i < 4 ; i += 1) {
+                    spanText.classList.add(randomClassGenerator()[i]);
+                }
+            });
+            viewLetter.appendChild(spanText);
+            }            
+        }
+    
+}
+
+function clearAll(){
+    let checkIfThereIsAnyTag = document.getElementsByTagName('span');
+    let checkIfThereIsAnyError = document.getElementById('errorID');
+    if (checkIfThereIsAnyError !== null){
+        checkIfThereIsAnyError.remove();
+    }
+    while(checkIfThereIsAnyTag.length !== 0){
+        checkIfThereIsAnyTag[0].remove();
     }
 }
 
