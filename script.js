fbb8579f406b48aeb22556b8f8f9300d39ddabe2
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-05 20:17:24 -0300

    4. Tag span + split palavra

---
+++ script.js
@@ -0,0 +1,31 @@
+const text = document.querySelector('#carta-texto');
+const divText = document.querySelector('#carta-gerada');
+let arr = [];
+
+function splitSentence() {
+  let valor = text.value;
+  palavra = '';
+  for (let index = 0; index < valor.length; index += 1) {
+    if (valor[index] !== ' ') {
+      palavra += valor[index];
+    } else {
+      arr.push(palavra);
+      palavra = '';
+    }
+  }
+  arr.push(palavra);
+  return arr;
+}
+
+function populateSpan() {
+  splitSentence();
+  for (let index = 0; index < arr.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerHTML = `${arr[index]} `;
+    divText.appendChild(span);
+  }
+}
+
+window.onload = function () {
+  document.querySelector('#criar-carta').addEventListener('click', populateSpan);
+}
