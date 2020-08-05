0119648045f2da7eb18ac775e0b742f7b3ac2317
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-05 20:25:27 -0300

    3. Consertando o output

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const text = document.querySelector('#carta-texto');
 const divText = document.querySelector('#carta-gerada');
-let arr = [];
+const arr = [];
 
 function splitSentence() {
-  let valor = text.value;
-  palavra = '';
+  const valor = text.value;
+  let palavra = '';
   for (let index = 0; index < valor.length; index += 1) {
     if (valor[index] !== ' ') {
       palavra += valor[index];
@@ -21,11 +21,11 @@ function populateSpan() {
   splitSentence();
   for (let index = 0; index < arr.length; index += 1) {
     const span = document.createElement('span');
-    span.innerHTML = `${arr[index]} `;
+    span.innerHTML = `${arr[index]}`;
     divText.appendChild(span);
   }
 }
 
 window.onload = function () {
   document.querySelector('#criar-carta').addEventListener('click', populateSpan);
-}
+};
