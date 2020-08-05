0639a188ce6822bfc92d455e553bb89e75689a91
Dilenio Enderle <dilenio@gmail.com>
2020-08-05 09:55:56 -0300

    4 - Ao criar uma carta através do botão id=criar-carta, o input com id=carta-texto deve permanecer com o texto digitado.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Mistery Letter
+// Apaga todas as palavras da carta gerada
+function removeAllWords() {
+  const allWords = document.querySelector('#carta-gerada');
+  while (allWords.firstChild) {
+    allWords.removeChild(allWords.lastChild);
+  }
+}
+
+// Cria palavras da carta
+function createWords() {
+  const addWord = document.querySelector('#carta-gerada');
+  const inputText = document.querySelector('#carta-texto');
+  const arrayWords = inputText.value.split(' ');
+  removeAllWords();
+  for (let index = 0; index < arrayWords.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerText = arrayWords[index];
+    addWord.appendChild(span);
+  }
+}
+
+const createLetter = document.querySelector('#criar-carta');
+createLetter.addEventListener('click', createWords);
+
