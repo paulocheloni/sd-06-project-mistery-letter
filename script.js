c42376efd5c57538815d0eb762ac80a937a23f6e
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-06 02:35:52 -0300

    Add a counter of words

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const generateLetter = document.getElementById('carta-gerada');
 const btnGenerateLetter = document.getElementById('criar-carta');
 const inputLetter = document.getElementById('carta-texto');
+const countLetter = document.getElementById('carta-contador');
+let counter = 0;
+
+const classesGroup = ['newspaper', 'magazine', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
 
 function verifyMessage() {
   if (inputLetter.value === '' || inputLetter.value.trim() === '') {
@@ -9,15 +13,23 @@ function verifyMessage() {
   return true;
 }
 
+function randomClasses() {
+  const random = Math.floor(Math.random() * 10);
+  return classesGroup[random];
+}
+
 function createLetter() {
   if (verifyMessage() === true) {
     let inputWords = inputLetter.value;
     inputWords = inputWords.split(' ');
     for (let index = 0; index < inputWords.length; index += 1) {
       const createWords = document.createElement('span');
+      createWords.className = randomClasses();
       generateLetter.appendChild(createWords);
       createWords.innerHTML = inputWords[index];
+      counter += 1;
     }
+    countLetter.innerHTML = counter;
   }
 }
 
