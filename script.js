2c75082d27025b942adf88f917f16c368a66a67c
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-11 12:59:32 -0300

    6 - 16. Classes de forma aleatória

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const text = document.querySelector('#carta-texto');
 const divText = document.querySelector('#carta-gerada');
 let palavra = [];
+const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
+const grupoTamanho = ['medium', 'big', 'reallybig'];
+const grupoRotacao = ['rotateleft', 'rotateright'];
+const grupoInclinacao = ['skewleft', 'skewright'];
 
 const splitSentence = () => {
   const valor = text.value;
@@ -15,6 +19,11 @@ const clearFields = () => {
   }
 };
 
+const sorteia = (arr) => {
+  const pos = Math.floor(Math.random() * arr.length);
+  return arr[pos];
+};
+
 function populateSpan() {
   splitSentence();
   if (palavra[0] === '') {
@@ -25,6 +34,11 @@ function populateSpan() {
       const span = document.createElement('span');
       span.innerHTML = `${palavra[index]}`;
       divText.appendChild(span);
+      const classesEstilo = sorteia(grupoEstilo);
+      const classesTamanho = sorteia(grupoTamanho);
+      const classesRotacao = sorteia(grupoRotacao);
+      const classesInclinacao = sorteia(grupoInclinacao);
+      span.className = `${classesEstilo} ${classesTamanho} ${classesRotacao} ${classesInclinacao}`;
     }
   }
 }
