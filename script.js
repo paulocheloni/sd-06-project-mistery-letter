c2ec58427f561fcdcb7a9535fd6c015d46be9692
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-11 13:22:32 -0300

    6 - 18. Contador de palavras

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
 const grupoTamanho = ['medium', 'big', 'reallybig'];
 const grupoRotacao = ['rotateleft', 'rotateright'];
 const grupoInclinacao = ['skewleft', 'skewright'];
+const contador = document.querySelector('#carta-contador');
+let total = 0;
 
 const splitSentence = () => {
   const valor = text.value;
   palavra = valor.split(' ');
+  total = palavra.length;
   return palavra;
 };
 
@@ -28,6 +31,7 @@ function populateSpan() {
   splitSentence();
   if (palavra[0] === '') {
     divText.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    contador.innerHTML = '';
   } else {
     clearFields();
     for (let index = 0; index < palavra.length; index += 1) {
@@ -40,9 +44,20 @@ function populateSpan() {
       const classesInclinacao = sorteia(grupoInclinacao);
       span.className = `${classesEstilo} ${classesTamanho} ${classesRotacao} ${classesInclinacao}`;
     }
+    contador.innerHTML = total;
   }
 }
 
+const changeClass = (event) => {
+  const click = event.target;
+  const classesEstilo = sorteia(grupoEstilo);
+  const classesTamanho = sorteia(grupoTamanho);
+  const classesRotacao = sorteia(grupoRotacao);
+  const classesInclinacao = sorteia(grupoInclinacao);
+  click.className = `${classesEstilo} ${classesTamanho} ${classesRotacao} ${classesInclinacao}`;
+};
+
 window.onload = function () {
   document.querySelector('#criar-carta').addEventListener('click', populateSpan);
+  divText.addEventListener('click', changeClass);
 };
